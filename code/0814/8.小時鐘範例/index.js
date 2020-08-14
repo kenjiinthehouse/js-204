3e8fa6ec85931c84fbcb94fbd4fac29edd0e8311
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-08-14 14:16:16 +0800

    Update index.js

--- code/0814/8.小時鐘範例/index.js
+++ code/0814/8.小時鐘範例/index.js
@@ -6,9 +6,12 @@ const displayTime = () => {
 let timerId = null
 
 document.getElementById('start').addEventListener('click', function () {
+  // timerId是一個整數值，可用於控制計時器關閉
   timerId = setInterval(displayTime, 1000)
+  //console.log(timerId)
 })
 
 document.getElementById('stop').addEventListener('click', function () {
+  // 清除計時器代表停止某個對應的計數器
   clearInterval(timerId)
 })
