bb565df99997a8d68854b601b1db795fe3afd31d
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-08-18 09:39:44 +0800

    up

---
+++ code/0818/1.async-await語法範例/index.js
@@ -0,0 +1,43 @@
+const getDataFromServer = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:5500/public/data.json', {
+      method: 'GET',
+    })
+
+    const data = await response.json()
+
+    // 得到data資料，顯示在網頁上
+    document.getElementById('data-list').innerHTML = data
+      .map((v) => {
+        return `<li>${v.text}</li>`
+      })
+      .join('')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getDataFromServer()
+
+// // 連接伺服器，同一台電腦上
+// fetch('http://127.0.0.1:5500/public/data.json', { method: 'GET' })
+//   .then((response) => {
+//     console.log(response)
+
+//     // 得到response物件，回傳剖折出json資料
+//     return response.json()
+//   })
+//   .then((data) => {
+//     console.log(data)
+
+//     // 得到data資料，顯示在網頁上
+//     document.getElementById('data-list').innerHTML = data
+//       .map((v) => {
+//         return `<li>${v.text}</li>`
+//       })
+//       .join('')
+//   })
+//   .catch((error) => {
+//     // 有錯誤在這裡處理
+//     console.log(error)
+//   })
