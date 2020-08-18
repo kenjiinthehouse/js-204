a6df14ee4b539705e53ebd38cf896850783d31e9
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-08-18 10:49:11 +0800

    Update index.js

--- code/0818/3.計數器-與json-server連接/index.js
+++ code/0818/3.計數器-與json-server連接/index.js
@@ -43,23 +43,23 @@ const putDataToServer = async (total) => {
   try {
     const newData = { total }
 
-    // 連接伺服器，同一台電腦上，得到回應值
+    // 連接伺服器
     const response = await fetch('http://localhost:3000/counter/1', {
+      // 使用的方法
       method: 'PUT',
+      // 指示此資料為json格式
       headers: new Headers({
         Accept: 'application/json',
         'Content-Type': 'application/json',
       }),
+      // 傳送的主體資料(json格式)
       body: JSON.stringify(newData),
     })
 
     if (!response.ok) throw new Error('Server has error')
 
-    // 由response物件，剖析出json資料
+    // 目前沒用到，由response物件，剖析出json資料
     const data = await response.json()
-
-    // 得到data資料，顯示在網頁上
-    //document.getElementById('total').innerHTML = data.total
   } catch (error) {
     console.log(error)
   }
@@ -69,12 +69,16 @@ getDataFromServer()
 
 byId('add').addEventListener('click', function () {
   const newTotal = +byId('total').innerHTML + 1
+  // 送到伺服器作更新
   putDataToServer(newTotal)
+  // 在網頁上作更新
   byId('total').innerHTML = newTotal
 })
 
 byId('minus').addEventListener('click', function () {
   const newTotal = +byId('total').innerHTML - 1
+  // 送到伺服器作更新
   putDataToServer(newTotal)
+  // 在網頁上作更新
   byId('total').innerHTML = newTotal
 })
